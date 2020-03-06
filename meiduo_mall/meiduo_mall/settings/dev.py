8fbbe499701a2fab64047b641ed6f2a7c162b2e2
Hyman-Yuan <1197850509@qq.com>
2020-03-06 15:21:28 +0800

    set redis database

--- meiduo_mall/meiduo_mall/settings/dev.py
+++ meiduo_mall/meiduo_mall/settings/dev.py
@@ -119,6 +119,26 @@ USE_L10N = True
 
 USE_TZ = True
 
+CACHES = {
+    'default':{
+        'BACKENDS':'django_redis.cache.RedisCache',
+        'LOCATION':'redis://127.0.0.1:6379/0',
+        'OPTIONS':{
+            'CLIENT_CLASS':'django_redis.client.DefaultClient'
+        }
+    },
+    'session':{
+        'BACKENDS':'django_redis.cache.RedisCache',
+        'LOCATION':'redis://127.0.0.1:6379/1',
+        'OPTIONS':{
+            'CLIENT_CLASS':'django_redis.client.DefaultClient'
+        }
+    },
+}
+
+SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
+
+SESSION_CACHE_ALIAS = 'session'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
