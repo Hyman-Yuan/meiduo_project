81ed5a659b232008b003575ea994e269db8a5726
Hyman-Yuan <1197850509@qq.com>
2020-03-17 11:16:38 +0800

    'User模型补充email_active字段'

--- meiduo_mall/meiduo_mall/apps/user/models.py
+++ meiduo_mall/meiduo_mall/apps/user/models.py
@@ -8,6 +8,7 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
 
     user_mobile = models.CharField(max_length=11,unique=True,verbose_name='手机号码')
+    email_active = models.BooleanField(default=False,verbose_name='邮箱验证状态')
 
     class Meta:
         db_table = 'tb_users'
