853b5aede243557d87dcc1c98d3dd606ca600345
Hyman-Yuan <1197850509@qq.com>
2020-03-23 14:15:44 +0800

    商品列表的路由 和视图函数

--- meiduo_mall/meiduo_mall/apps/goods/urls.py
+++ meiduo_mall/meiduo_mall/apps/goods/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
-
+from . import views
 urlpatterns = [
+    # /list/115/1/
+    url(r'^list/(?P<category_id>\d+)/(?P<page_num>\d+)/$',views.ListView.as_view()),
 
 ]
--- meiduo_mall/meiduo_mall/apps/goods/views.py
+++ meiduo_mall/meiduo_mall/apps/goods/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.views import View
 # Create your views here.
+
+
+class ListView(View):
+    def get(self,category_id,page_num):
+        pass
