9848e07ce5cd4bf27d2b995760acb1e8707d6c20
Hyman-Yuan <1197850509@qq.com>
2020-03-25 14:27:19 +0800

    统计分类商品访问量模型类

--- meiduo_mall/meiduo_mall/apps/goods/urls.py
+++ meiduo_mall/meiduo_mall/apps/goods/urls.py
@@ -3,5 +3,8 @@ from . import views
 urlpatterns = [
     # /list/115/1/
     url(r'^list/(?P<category_id>\d+)/(?P<page_num>\d+)/$',views.ListView.as_view()),
+    url(r'^hot/(?P<category_id>\d+)/$',views.HotGoodsView.as_view()),
+    url(r'^detail/(?P<sku_id>\d+)/$',views.DetailView.as_view()),
+
 
 ]
